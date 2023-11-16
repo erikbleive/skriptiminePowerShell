c48c977247d1df5f530bbda9cc8f60a08763d03d
erik bleive <erik.bleive@voco.ee>
2023-11-16 04:28:26 -0800

    praks1 - lahendus

---
+++ pr1.ps1
@@ -0,0 +1,12 @@
+﻿$numbers1 = @(1, 2, 3)
+$numbers2 = @(1, 2, 3)
+$el1 = numbers1 [0] + $numbers2 [0]
+$el1 = numbers1 [1] + $numbers2 [1]
+$el1 = numbers1 [2] + $numbers2 [2]
+
+$numbers3 = @($el1, $el2, $el3) # variant_1
+$numbers3 = $numbers3 + $el1
+$numbers3 = $numbers3 + $el2
+$numbers3 += $el3
+
+$numbers3

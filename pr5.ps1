09944f54886f580f6174fd81d8a14c076bade3c2
erik bleive <erik.bleive@voco.ee>
2023-11-23 05:21:40 -0800

    pr5

---
+++ pr5.ps1
@@ -0,0 +1,11 @@
+﻿Get-ChildItem -path "C:\temp\test"
+
+$file = Get-ChildItem -Path "C:\temp\test" | where {$_.Name -like "*.csv"} | select Name, Length
+$sizeinKB = $file.Length/1KB
+$sizeinMB = $file.Length/1MB
+
+Write-Host "nFileName : "$file.name
+Write-Host "Size in KB : "$sizeinKB
+Write-Host "Size in MB : "$sizeinMB
+
+Get-ChildItem -path "C:\temp\test"

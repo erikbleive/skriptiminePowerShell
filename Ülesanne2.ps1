494feebb43dc4cccb80cc37bd5af20d59a279942
erik bleive <erik.bleive@voco.ee>
2023-11-23 04:34:17 -0800

    ül2

---
+++ Ülesanne2.ps1
@@ -0,0 +1,14 @@
+﻿# Küsi kasutaja eesnime ja perekonnanime
+$eesnimi = Read-Host -Prompt "Sisesta kasutaja eesnimi"
+$perekonnanimi = Read-Host -Prompt "Sisesta kasutaja perekonnanimi"
+
+#(kasutades eesnime esimest tähte ja perekonnanime)
+$kasutajanimi = "$($eesnimi.ToLower()).$($perekonnanimi.ToLower())"
+
+# Kustutame soovitud kasutaja
+ Remove-Localuser -Name $kasutajanimi -Verbose
+ if ($?) { 
+  Write-Host "Kasutaja $kasutajanimi on edukalt kustutatud."
+ }   else {
+    Write-Host "Tekkis probleeme kustutamisega"
+}

937a24dbac4a1b9bc438945f9b0c6cb9999f86c7
erik bleive <erik.bleive@voco.ee>
2023-11-23 03:59:57 -0800

    ül1

---
+++ Ülesanne1.ps1
@@ -0,0 +1,15 @@
+﻿# Kasutaja eesnimi
+$eesnimi = Read-Host "Sisesta oma eesnimi"
+
+# Kasutaja perekonnanimi
+$perekonnanimi = Read-Host "Sisesta oma perekonnanimi "
+
+# Loome kasutajanime eesnime ja perekonnanime vahelise punktiga, kasutades ToLower() funktsiooni
+$kasutajanimi = "$($eesnimi.ToLower()).$($perekonnanimi.ToLower())"
+New-LocalUser -Name $kasutajanimi
+
+# Väljastame kasutajanime
+Write-Host "Kasutajanimi: $kasutajanimi"
+
+# Anname nüüd kasutajale parooli (Parool1!)
+$parool =  ConvertTo-SecureString "Parool1!" -AsPlainText -Force
